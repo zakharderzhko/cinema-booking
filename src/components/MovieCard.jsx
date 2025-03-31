76184d5b14ff16eec3e9baae46787b70dd490231
Zakhar Derzhko bc2024-6 <zakhar.derzhko@lnu.edu.ua>
2025-03-31 13:11:50 +0300

    Створено та реалізовано компонент MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
- 
+import React from 'react';
+
+const MovieCard = ({ movie }) => {
+  return (
+    <div className="movie-card">
+      <img src={movie.poster} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>{movie.description}</p>
+      <p>Genre: {movie.genre}</p>
+      <p>Time: {movie.time}</p>
+    </div>
+  );
+};
+
+export default MovieCard;
+
