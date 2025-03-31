c85254c1123a4c5f839c18df21a29b0dbcd376b1
Zakhar Derzhko bc2024-6 <zakhar.derzhko@lnu.edu.ua>
2025-03-31 13:32:07 +0300

    Створено та реалізовано файл movies.js. Додано список із трьох фільмів.

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,1 +1,26 @@
- 
+const movies = [
+    {
+      title: 'Snow White',
+      description: "Mirror, Mirror on the wall... who's the fairest one of all",
+      genre: 'drama, romance, musical, adventure, family, fantasy',
+      time: '3:50 PM, 2025-03-31',
+      poster: '/images/snowwhite.jpg',
+    },
+    {
+      title: 'Blind vacation',
+      description: "A vacation turns the characters' lives upside down and changes everything forever.",
+      genre: 'comedy, melodrama',
+      time: '11:50 AM, 2025-04-01',
+      poster: '/images/blindvacation.jpg',
+    },
+    {
+        title: 'Dogs',
+        description: "She turns men into dogs. And they turn her into a human.",
+        genre: 'comedy, romance',
+        time: '10:00 AM, 2025-04-02',
+        poster: '/images/dogs.jpg',
+      }
+  ];
+  
+  export default movies;
+  
