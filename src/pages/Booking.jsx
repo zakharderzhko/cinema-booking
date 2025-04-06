bec4fd419fe5b9def0404419f84463a961559aee
Zakhar Derzhko bc2024-6 <zakhar.derzhko@lnu.edu.ua>
2025-04-06 19:02:28 +0300

    Створено сторінку Booking.jsx для відображення кінозалу з можливістю вибору місць.

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import CinemaHall from '../components/CinemaHall';
+
+const Booking = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="app">
+      <h2>Бронювання: {id}</h2>
+      <CinemaHall />
+    </div>
+  );
+};
+
+export default Booking;
  
